10782b501e6839056ef927bbb0b5bdb7a3c34045
FranSilva <fransilvamartin@gmail.com>
2022-09-21 00:37:46 +0200

    :sparkles: Creación de la playerData al entrar el jugador

--- src/main/java/dev/fransilva/hydrogen/listeners/PlayerJoin.java
+++ src/main/java/dev/fransilva/hydrogen/listeners/PlayerJoin.java
@@ -1,5 +1,10 @@
 package dev.fransilva.hydrogen.listeners;
 
+import dev.fransilva.hydrogen.managers.ConfigManager;
+import dev.fransilva.hydrogen.managers.PlayerDataManager;
+import dev.fransilva.hydrogen.utils.TextUtils;
+import org.bukkit.Location;
+import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -7,25 +12,53 @@ import org.bukkit.event.player.PlayerJoinEvent;
 
 import dev.fransilva.hydrogen.Hydrogen;
 import net.md_5.bungee.api.ChatColor;
+import org.bukkit.inventory.ItemStack;
 
 public class PlayerJoin implements Listener {
-	
-	@SuppressWarnings("unused")
-	private Hydrogen hydrogen;
-	private Player player;
-	private String playerName;
-	private String message;
-	
+
+    @SuppressWarnings("unused")
+    private Hydrogen hydrogen;
+    private Player player;
+    private String playerName;
+    private String message;
+    private ConfigManager configManager;
+    private PlayerDataManager playerDataManager;
+
     public PlayerJoin(Hydrogen plugin) {
         this.hydrogen = plugin;
+        this.configManager = ConfigManager.getInstance();
+        this.playerDataManager = new PlayerDataManager(configManager);
     }
-	
-	@EventHandler
+
+    @EventHandler
     public void onJoin(PlayerJoinEvent event) {
-		player = event.getPlayer();
-		playerName = player.getName();
-		message = ChatColor.GRAY + "Se ha unido el jugador " + ChatColor.WHITE + playerName;
-		
-		event.setJoinMessage(message);
+        player = event.getPlayer();
+        playerName = player.getName();
+        message = ChatColor.GRAY + "Se ha unido el jugador " + ChatColor.WHITE + playerName;
+
+        event.setJoinMessage(message);
+    }
+
+    @EventHandler
+    public void playerData(PlayerJoinEvent event) {
+        player = event.getPlayer();
+        playerDataManager.setupPlayerData(player);
+        event.setJoinMessage(null);
+    }
+
+    @EventHandler
+    public void firstJoinEvent(PlayerJoinEvent event) {
+        Player player = event.getPlayer();
+
+        if (!player.hasPlayedBefore()) {
+            Location location = configManager.getLocation("config.yml", "spawn");
+
+            if (location != null) {
+                player.teleport(location);
+                player.sendMessage(configManager.getMessage("teleport_spawn"));
+            } else {
+                player.sendMessage(configManager.getMessage("spawn_not_found"));
+            }
+        }
     }
 }

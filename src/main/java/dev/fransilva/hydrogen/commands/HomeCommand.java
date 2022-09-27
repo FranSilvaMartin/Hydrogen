bc5604e3d0aa4707186cb61279167e17ad982e1d
Francisco Silva Martín <fransilvamartin@gmail.com>
2022-09-27 09:13:10 +0200

    :hammer::fire: Líneas sin uso en HomeCommand + registro de comandos en Hydrogen

--- src/main/java/dev/fransilva/hydrogen/commands/HomeCommand.java
+++ src/main/java/dev/fransilva/hydrogen/commands/HomeCommand.java
@@ -3,6 +3,7 @@ package dev.fransilva.hydrogen.commands;
 import dev.fransilva.hydrogen.Hydrogen;
 import dev.fransilva.hydrogen.managers.ConfigManager;
 import dev.fransilva.hydrogen.managers.CountdownManager;
+import dev.fransilva.hydrogen.utils.CheckUtils;
 import dev.fransilva.hydrogen.utils.TextUtils;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
@@ -36,52 +37,49 @@ public class HomeCommand implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
 
-        if (sender instanceof Player) {
-            player = (Player) sender;
+        if (CheckUtils.verifyIfIsAPlayer(sender, true)) {
+            if (CheckUtils.hasPermission(sender, command.getName())) {
+                player = (Player) sender;
 
+                list = new ArrayList<String>();
+                ConfigurationSection sec = configManager.getConfig("userdata/" + player.getUniqueId() + ".yml").getConfigurationSection("homes");
 
-            list = new ArrayList<String>();
-            ConfigurationSection sec = configManager.getConfig("userdata/" + player.getUniqueId() + ".yml").getConfigurationSection("homes");
-
-            if (sec != null) {
-                for (String warpName : sec.getKeys(false)) {
-                    list.add(warpName);
-                }
-            }
-
-            if (hydrogen.lista.contains(player.getUniqueId())) {
-                return true;
-            }
-
-            if (args.length == 0) {
-                if (list.isEmpty()) {
-                    player.sendMessage(TextUtils.colorize("&cNo hay ningun warp asignado, usa /sethome <name>"));
-                } else {
-                    String warpList = String.join(", ", list);
-                    player.sendMessage(TextUtils.colorize("&cHome list: " + warpList));
+                if (sec != null) {
+                    for (String warpName : sec.getKeys(false)) {
+                        list.add(warpName);
+                    }
                 }
 
+                if (hydrogen.lista.contains(player.getUniqueId())) {
+                    return true;
+                }
+
+                if (args.length == 0) {
+                    if (list.isEmpty()) {
+                        player.sendMessage(TextUtils.colorize("&cNo hay ningun warp asignado, usa /sethome <name>"));
+                    } else {
+                        String warpList = String.join(", ", list);
+                        player.sendMessage(TextUtils.colorize("&cHome list: " + warpList));
+                    }
+
+                    return true;
+                }
+                String homeSelected = args[0];
+                homeSelected = homeSelected.substring(0,1).toUpperCase() + homeSelected.substring(1).toLowerCase();
+
+                if (!checkIfHomeExists(homeSelected)) {
+                    player.sendMessage(TextUtils.colorize("&cEl home &e" + homeSelected + " &cno existe!"));
+                    return true;
+                }
+
+                hydrogen.lista.add(player.getUniqueId());
+
+                location = configManager.getLocation("userdata/" + player.getUniqueId() + ".yml", "homes." + homeSelected);
+                new CountdownManager(hydrogen, player, location, countdown, "home " + homeSelected);
                 return true;
             }
-            String homeSelected = args[0];
-            homeSelected = homeSelected.substring(0,1).toUpperCase() + homeSelected.substring(1).toLowerCase();
-
-            if (!checkIfHomeExists(homeSelected)) {
-                player.sendMessage(TextUtils.colorize("&cEl home &e" + homeSelected + " &cno existe!"));
-                return true;
-            }
-
-            if (hydrogen.lista.contains(player.getUniqueId())) {
-                return true;
-            }
-            hydrogen.lista.add(player.getUniqueId());
-
-            location = configManager.getLocation("userdata/" + player.getUniqueId() + ".yml", "homes." + homeSelected);
-            new CountdownManager(hydrogen, player, location, countdown, "home " + homeSelected);
-            return true;
         }
-
-        return false;
+        return true;
     }
 
     private boolean checkIfHomeExists(String warpSelected) {
